d05108bb16766546697d88119655a66cca7f6292
Codieeeeee <siddharthabojanki@gmail.com>
2025-06-28 00:21:36 +0530

    Fixed bug related to cookies

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,7 +22,7 @@ export const register = async (req, res) => {
         res.cookie('token', token ,{
             httpOnly: true,  // Prevent javascript to access cookie
             secure: process.env.NODE_ENV === 'production',  // Use secure cookies in production
-            sameSite: process.env.NODE_ENV === 'production' ? none : 'strict',  // CSRF protection
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',  // CSRF protection
             maxAge: 7 * 24 * 60 * 60 * 1000, // Cookie expiration time(7 days)
         })
 
@@ -54,7 +54,7 @@ export const login = async (req, res) => {
         res.cookie('token', token ,{
             httpOnly: true,  // Prevent javascript to access cookie
             secure: process.env.NODE_ENV === 'production',  // Use secure cookies in production
-            sameSite: process.env.NODE_ENV === 'production' ? none : 'strict',  // CSRF protection
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',  // CSRF protection
             maxAge: 7 * 24 * 60 * 60 * 1000, // Cookie expiration time(7 days)
         })
 
@@ -86,7 +86,7 @@ export const logout = async (req, res) => {
         res.clearCookie('token', {
             httpOnly: true, 
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? none : 'strict',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
         })
         return res.json({success: true, message: "Logged Out"})
     } 
