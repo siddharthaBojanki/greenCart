1579e3f6b0cc756d5de9e55f60630512c7319597
Codieeeeee <siddharthabojanki@gmail.com>
2025-06-27 23:04:22 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ await connectCloudinary();
 await connectDB();
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173', 'https://green-cart-taupe.vercel.app'];
 
 app.post('/stripe', express.raw({type: "application/json"}), stripeWebhooks);
 
